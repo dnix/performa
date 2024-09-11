8a7fb32681a3f18eedcaf239cc3c8806c7f794ba
dnix <2434250+dnix@users.noreply.github.com>
2024-09-11 15:38:11 -0400

    feat: init files for cleaner lib imports

---
+++ src/performa/__init__.py
@@ -0,0 +1,2 @@
+# This file is intentionally left empty.
+# Package initialization can be done in submodules as needed.
---
+++ src/performa/models/__init__.py
@@ -0,0 +1,43 @@
+# Import all model classes
+
+from .budget import Budget, BudgetItem
+from .cash_flow import CashFlowItem
+from .deal import CarryPromote, Deal, Partner, Promote, WaterfallPromote, WaterfallTier
+from .expense import Expense, ExpenseCostItem, ExpenseFactorItem, ExpenseItem
+from .financing import ConstructionFinancing, PermanentFinancing
+from .model import Model
+from .program import Program
+from .project import CapRates, Project
+from .revenue import (
+    RentalRevenueItem,
+    Revenue,
+    RevenueItem,
+    SalesRevenueItem,
+)
+
+# Define __all__ to specify what gets imported with "from performa.models import *"
+__all__ = [
+    "Model",
+    "CashFlowItem",
+    "BudgetItem",
+    "Budget",
+    "Program",
+    "RevenueItem",
+    "SalesRevenueItem",
+    "RentalRevenueItem",
+    "Revenue",
+    "ExpenseItem",
+    "ExpenseCostItem",
+    "ExpenseFactorItem",
+    "Expense",
+    "ConstructionFinancing",
+    "PermanentFinancing",
+    "Project",
+    "CapRates",
+    "Partner",
+    "Promote",
+    "WaterfallTier",
+    "WaterfallPromote",
+    "CarryPromote",
+    "Deal",
+]
