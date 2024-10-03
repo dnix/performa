d3cb151ffd239baa8dd24ef78f9a1284b1feec2f
dnix <2434250+dnix@users.noreply.github.com>
2024-10-02 20:17:23 -0400

    docs: comments

--- src/performa/utils/pandas_settings.py
+++ src/performa/utils/pandas_settings.py
@@ -1,4 +1,3 @@
-# pandas_settings.py
 import pandas as pd
 
 from .money import Money, MoneyDtype
@@ -8,10 +7,12 @@ class PandasSettings:
     @staticmethod
     def apply_custom_settings():
         """Apply custom pandas settings."""
+        # Set the display format for floating-point numbers
         pd.set_option(
             "display.float_format",
             lambda x: f"${x:.2f}" if isinstance(x, (float, Money)) else str(x),
         )
+        # Register the MoneyDtype with pandas
         pd.api.extensions.register_extension_dtype(MoneyDtype)
         # NOTE: see money.py for more Money related configuration
 
