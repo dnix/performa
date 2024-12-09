55893a327b0891bb388d23c406f82b975926c088
dnix <2434250+dnix@users.noreply.github.com>
2024-12-09 15:07:55 -0500

    chore: revert decimal types

--- src/performa/utils/_types.py
+++ src/performa/utils/_types.py
@@ -2,12 +2,8 @@ from typing import Annotated
 
 from pydantic import Field
 
-from ._decimal import DecimalExtensionArray
-
 # constrained types
 PositiveInt = Annotated[int, Field(strict=True, ge=0)]
 PositiveIntGt1 = Annotated[int, Field(strict=True, gt=1)]
 PositiveFloat = Annotated[float, Field(strict=True, ge=0)]
 FloatBetween0And1 = Annotated[float, Field(strict=True, ge=0, le=1)]
-PositiveDecimal = Annotated[DecimalExtensionArray, Field(strict=True, ge=0)]
-NegativeDecimal = Annotated[DecimalExtensionArray, Field(strict=True, lt=0)]
