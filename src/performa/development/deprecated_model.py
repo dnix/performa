5dd0e3d056fb0977f3db5bada08f892f1ee156d1
dnix <2434250+dnix@users.noreply.github.com>
2025-07-02 00:09:02 -0400

    chore: remove deprecated development modules

--- src/performa/development/deprecated_model.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, ConfigDict
-
-
-class Model(BaseModel):
-    model_config = ConfigDict(
-        arbitrary_types_allowed=True,
-        use_enum_values=True,
-        # validate_assignment=True,  # TODO: evaluate if this is needed
-    )
